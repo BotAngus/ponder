f2546aa06af192e8b86fa9c608c6fbb20be8d0df
Angus <superangus@protonmail.com>
2024-11-03 17:27:56 +0100

    Added clone bound to parser

--- src/parser.rs
+++ src/parser.rs
@@ -3,12 +3,12 @@ use crate::{error::Error, span::Span, Input, Parser};
 pub fn just<'src, I, O, S, E: Error<I>>(expect: I, give: O) -> impl Parser<'src, I, O, S, E>
 where
     S: Span<'src>,
-    O: Copy,
+    O: Clone,
     I: Input<'src>,
     E: Error<I, Span<'src> = S>,
 {
     move |tokens| match tokens {
-        [(tok, span), tail @ ..] if *tok == expect => Ok((tail, (give, *span))),
+        [(tok, span), tail @ ..] if *tok == expect => Ok((tail, (give.clone(), *span))),
         [(tok, span), ..] => Err(E::unexpected(Some(*tok), *tok, *span)),
         [] => Err(E::eof(Some(expect))),
     }
